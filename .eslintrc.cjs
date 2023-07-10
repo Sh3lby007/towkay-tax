5ba1896550783b934dc41f058e6f3e3ae17e3961
shelby <>
2023-07-10 10:11:14 +0800

    Format whole project with new prettier settings.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,15 +8,15 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
-  root: true,
+    root: true,
 
-  extends: [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    extends: [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript',
+        '@vue/eslint-config-prettier/skip-formatting'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    }
 }
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
-<!-- <template>
-  <div class="wrapper">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </div>
-
-
-</template> -->
-
 <template>
-  <div class="wrapper card relative z-2">
-    <Menubar :model="items">
-      <template #start>
-        <img
-          alt="logo"
-          src="https://primefaces.org/cdn/primevue/images/logo.svg"
-          height="40"
-          class="mr-2"
-        />
-      </template>
-      <template #end>
-        <InputText placeholder="Search" type="text" />
-      </template>
-    </Menubar>
-  </div>
-  <RouterView />
+    <div class="wrapper card relative z-2">
+        <Menubar :model="items">
+            <template #start>
+                <img
+                    alt="logo"
+                    src="https://primefaces.org/cdn/primevue/images/logo.svg"
+                    height="40"
+                    class="mr-2"
+                />
+            </template>
+            <template #end>
+                <InputText placeholder="Search" type="text" />
+            </template>
+        </Menubar>
+    </div>
+    <RouterView />
 </template>
 
 <script setup lang="ts">
@@ -34,190 +23,190 @@ import Menubar from 'primevue/menubar'
 import { ref } from 'vue'
 
 const items = ref([
-  {
-    label: 'File',
-    icon: 'pi pi-fw pi-file',
-    items: [
-      {
-        label: 'New',
-        icon: 'pi pi-fw pi-plus',
+    {
+        label: 'File',
+        icon: 'pi pi-fw pi-file',
         items: [
-          {
-            label: 'Bookmark',
-            icon: 'pi pi-fw pi-bookmark'
-          },
-          {
-            label: 'Video',
-            icon: 'pi pi-fw pi-video'
-          }
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-plus',
+                items: [
+                    {
+                        label: 'Bookmark',
+                        icon: 'pi pi-fw pi-bookmark'
+                    },
+                    {
+                        label: 'Video',
+                        icon: 'pi pi-fw pi-video'
+                    }
+                ]
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-trash'
+            },
+            {
+                separator: true
+            },
+            {
+                label: 'Export',
+                icon: 'pi pi-fw pi-external-link'
+            }
         ]
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-fw pi-trash'
-      },
-      {
-        separator: true
-      },
-      {
-        label: 'Export',
-        icon: 'pi pi-fw pi-external-link'
-      }
-    ]
-  },
-  {
-    label: 'Edit',
-    icon: 'pi pi-fw pi-pencil',
-    items: [
-      {
-        label: 'Left',
-        icon: 'pi pi-fw pi-align-left'
-      },
-      {
-        label: 'Right',
-        icon: 'pi pi-fw pi-align-right'
-      },
-      {
-        label: 'Center',
-        icon: 'pi pi-fw pi-align-center'
-      },
-      {
-        label: 'Justify',
-        icon: 'pi pi-fw pi-align-justify'
-      }
-    ]
-  },
-  {
-    label: 'Users',
-    icon: 'pi pi-fw pi-user',
-    items: [
-      {
-        label: 'New',
-        icon: 'pi pi-fw pi-user-plus'
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-fw pi-user-minus'
-      },
-      {
-        label: 'Search',
-        icon: 'pi pi-fw pi-users',
-        items: [
-          {
-            label: 'Filter',
-            icon: 'pi pi-fw pi-filter',
-            items: [
-              {
-                label: 'Print',
-                icon: 'pi pi-fw pi-print'
-              }
-            ]
-          },
-          {
-            label: 'List',
-            icon: 'pi pi-fw pi-bars'
-          }
-        ]
-      }
-    ]
-  },
-  {
-    label: 'Events',
-    icon: 'pi pi-fw pi-calendar',
-    items: [
-      {
+    },
+    {
         label: 'Edit',
         icon: 'pi pi-fw pi-pencil',
         items: [
-          {
-            label: 'Save',
-            icon: 'pi pi-fw pi-calendar-plus'
-          },
-          {
-            label: 'Delete',
-            icon: 'pi pi-fw pi-calendar-minus'
-          }
+            {
+                label: 'Left',
+                icon: 'pi pi-fw pi-align-left'
+            },
+            {
+                label: 'Right',
+                icon: 'pi pi-fw pi-align-right'
+            },
+            {
+                label: 'Center',
+                icon: 'pi pi-fw pi-align-center'
+            },
+            {
+                label: 'Justify',
+                icon: 'pi pi-fw pi-align-justify'
+            }
         ]
-      },
-      {
-        label: 'Archieve',
-        icon: 'pi pi-fw pi-calendar-times',
+    },
+    {
+        label: 'Users',
+        icon: 'pi pi-fw pi-user',
         items: [
-          {
-            label: 'Remove',
-            icon: 'pi pi-fw pi-calendar-minus'
-          }
+            {
+                label: 'New',
+                icon: 'pi pi-fw pi-user-plus'
+            },
+            {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-user-minus'
+            },
+            {
+                label: 'Search',
+                icon: 'pi pi-fw pi-users',
+                items: [
+                    {
+                        label: 'Filter',
+                        icon: 'pi pi-fw pi-filter',
+                        items: [
+                            {
+                                label: 'Print',
+                                icon: 'pi pi-fw pi-print'
+                            }
+                        ]
+                    },
+                    {
+                        label: 'List',
+                        icon: 'pi pi-fw pi-bars'
+                    }
+                ]
+            }
         ]
-      }
-    ]
-  },
-  {
-    label: 'Quit',
-    icon: 'pi pi-fw pi-power-off'
-  }
+    },
+    {
+        label: 'Events',
+        icon: 'pi pi-fw pi-calendar',
+        items: [
+            {
+                label: 'Edit',
+                icon: 'pi pi-fw pi-pencil',
+                items: [
+                    {
+                        label: 'Save',
+                        icon: 'pi pi-fw pi-calendar-plus'
+                    },
+                    {
+                        label: 'Delete',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            },
+            {
+                label: 'Archieve',
+                icon: 'pi pi-fw pi-calendar-times',
+                items: [
+                    {
+                        label: 'Remove',
+                        icon: 'pi pi-fw pi-calendar-minus'
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: 'Quit',
+        icon: 'pi pi-fw pi-power-off'
+    }
 ])
 </script>
 
 <style scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
+    line-height: 1.5;
+    max-height: 100vh;
 }
 
 .logo {
-  display: block;
-  margin: 0 auto 2rem;
+    display: block;
+    margin: 0 auto 2rem;
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+    width: 100%;
+    font-size: 12px;
+    text-align: center;
+    margin-top: 2rem;
 }
 
 nav a.router-link-exact-active {
-  color: var(--color-text);
+    color: var(--color-text);
 }
 
 nav a.router-link-exact-active:hover {
-  background-color: transparent;
+    background-color: transparent;
 }
 
 nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+    display: inline-block;
+    padding: 0 1rem;
+    border-left: 1px solid var(--color-border);
 }
 
 nav a:first-of-type {
-  border: 0;
+    border: 0;
 }
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+    header {
+        display: flex;
+        place-items: center;
+        padding-right: calc(var(--section-gap) / 2);
+    }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+    .logo {
+        margin: 0 2rem 0 0;
+    }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    header .wrapper {
+        display: flex;
+        place-items: flex-start;
+        flex-wrap: wrap;
+    }
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+    nav {
+        text-align: left;
+        margin-left: -1rem;
+        font-size: 1rem;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+        padding: 1rem 0;
+        margin-top: 1rem;
+    }
 }
 </style>
