544535c76473f35a7b18b7651c9e726f6e20cb03
Sh3lby007 <zhengshaobin@ymail.com>
2023-07-12 23:24:25 +0800

    Fix width for page, add back components folder.

--- src/App.vue
+++ src/App.vue
@@ -3,43 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-    <section
-        class="rounded-md border border-neutral-500 bg-neutral-700 hover:bg-neutral-600 transition-colors text-neutral-100 flex flex-row items-start cursor-pointer"
-    >
-        <img src="http://placekitten.com/100/100" alt="cat" class="py-4 pl-4 aspect-square" />
-
-        <p class="mx-4 my-3 leading-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
-    </section>
-
-    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-        <div class="md:flex">
-            <div class="md:shrink-0">
-                <img
-                    class="h-48 w-full object-cover md:h-full md:w-48"
-                    src="../public/favicon.ico"
-                    alt="Modern building architecture"
-                />
-            </div>
-            <div class="p-8">
-                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
-                    Company retreats
-                </div>
-                <a
-                    href="#"
-                    class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
-                    >Incredible accommodation for your team</a
-                >
-                <p class="mt-2 text-slate-500">
-                    Looking to take your team away on a retreat to enjoy awesome food and take in
-                    some sunshine? We have a list of places to do just that.
-                </p>
-            </div>
-        </div>
-    </div>
+    <RouterView />
 </template>
 
 <style scoped>
---
+++ src/components/IncomeTable.vue
@@ -0,0 +1,222 @@
+<template>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full text-center">
+                        <thead class="border-b">
+                            <tr>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Class
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Heading
+                                </th>
+                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
+                                    Heading
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="border-b">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Default
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-blue-100 border-blue-200">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Primary
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-purple-100 border-purple-200">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Secondary
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-green-100 border-green-200">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Success
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-red-100 border-red-200">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Danger
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-yellow-100 border-yellow-200">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Warning
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-indigo-100 border-indigo-200">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Info
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-gray-50 border-gray-200">
+                                <td
+                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Light
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                            <tr class="border-b bg-gray-800 boder-gray-900">
+                                <td
+                                    class="text-sm text-white font-medium px-6 py-4 whitespace-nowrap"
+                                >
+                                    Dark
+                                </td>
+                                <td
+                                    class="text-sm text-white font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                                <td
+                                    class="text-sm text-white font-light px-6 py-4 whitespace-nowrap"
+                                >
+                                    Cell
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <table class="min-w-full text-left text-sm font-light">
+                        <thead class="border-b font-medium dark:border-neutral-500">
+                            <tr>
+                                <th scope="col" class="px-6 py-4">#</th>
+                                <th scope="col" class="px-6 py-4">First</th>
+                                <th scope="col" class="px-6 py-4">Last</th>
+                                <th scope="col" class="px-6 py-4">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="border-b dark:border-neutral-500">
+                                <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
+                                <td class="whitespace-nowrap px-6 py-4">Mark</td>
+                                <td class="whitespace-nowrap px-6 py-4">Otto</td>
+                                <td class="whitespace-nowrap px-6 py-4">@mdo</td>
+                            </tr>
+                            <tr class="border-b dark:border-neutral-500">
+                                <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
+                                <td class="whitespace-nowrap px-6 py-4">Jacob</td>
+                                <td class="whitespace-nowrap px-6 py-4">Thornton</td>
+                                <td class="whitespace-nowrap px-6 py-4">@fat</td>
+                            </tr>
+                            <tr class="border-b dark:border-neutral-500">
+                                <td class="whitespace-nowrap px-6 py-4 font-medium">3</td>
+                                <td class="whitespace-nowrap px-6 py-4">Larry</td>
+                                <td class="whitespace-nowrap px-6 py-4">Wild</td>
+                                <td class="whitespace-nowrap px-6 py-4">@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
