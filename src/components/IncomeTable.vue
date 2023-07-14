92952048771b52f8642ee57388222136504e204d
Sh3lby007 <zhengshaobin@ymail.com>
2023-07-15 00:23:42 +0800

    Preliminary calculator done.

--- src/components/IncomeTable.vue
+++ src/components/IncomeTable.vue
@@ -1,236 +1,94 @@
 <template>
-    <div class="flex flex-col">
-        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="overflow-hidden">
-                    <table class="min-w-full text-center">
-                        <thead class="border-b">
-                            <tr>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                                    Class
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                                    Heading
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4">
-                                    Heading
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="border-b">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Default
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-blue-100 border-blue-200">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Primary
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-purple-100 border-purple-200">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Secondary
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-green-100 border-green-200">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Success
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-red-100 border-red-200">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Danger
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-yellow-100 border-yellow-200">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Warning
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-indigo-100 border-indigo-200">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Info
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-gray-50 border-gray-200">
-                                <td
-                                    class="text-sm text-gray-900 font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Light
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                            <tr class="border-b bg-gray-800 boder-gray-900">
-                                <td
-                                    class="text-sm text-white font-medium px-6 py-4 whitespace-nowrap"
-                                >
-                                    Dark
-                                </td>
-                                <td
-                                    class="text-sm text-white font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                                <td
-                                    class="text-sm text-white font-light px-6 py-4 whitespace-nowrap"
-                                >
-                                    Cell
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+    <div class="max-m-wd mx-auto p-4 mt-6 rounded-lg shadow-md border-lime-950">
+        <h1
+            ref="typingText"
+            class="overflow-hidden whitespace-nowrap font-mono text-xl font-bold animate-typing border-r-4"
+        >
+            Hello, world.
+        </h1>
+        <h2 class="text-lg font-medium mb-2">Income Tax Calculator</h2>
+
+        <div class="mb-4">
+            <label class="block text-gray-700 font-medium mb-2"> Annual Income </label>
+
+            <input v-model="income" type="number" class="border p-2 w-full rounded" />
+        </div>
+
+        <div class="mb-4">
+            <label class="block text-gray-700 font-medium mb-2"> Tax Rate </label>
+
+            <input v-model="taxRate" type="number" class="border p-2 w-full rounded" />
+        </div>
+
+        <button class="bg-blue-500 text-white py-2 px-4 rounded" @click="calculateTax">
+            Calculate Tax
+        </button>
+
+        <div v-if="isCalculated" class="mt-4">
+            <div class="text-lg font-medium mb-2">Tax Amount</div>
+
+            <div class="text-xl font-bold">
+                {{ taxAmount.toFixed(2) }}
             </div>
         </div>
     </div>
-    <div class="flex flex-col">
-        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
-                <div class="overflow-hidden">
-                    <table class="min-w-full text-left text-sm font-light">
-                        <thead class="border-b font-medium dark:border-neutral-500">
-                            <tr>
-                                <th scope="col" class="px-6 py-4">#</th>
-                                <th scope="col" class="px-6 py-4">First</th>
-                                <th scope="col" class="px-6 py-4">Last</th>
-                                <th scope="col" class="px-6 py-4">Handle</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr class="border-b dark:border-neutral-500">
-                                <td class="whitespace-nowrap px-6 py-4 font-medium">1</td>
-                                <td class="whitespace-nowrap px-6 py-4">Mark</td>
-                                <td class="whitespace-nowrap px-6 py-4">Otto</td>
-                                <td class="whitespace-nowrap px-6 py-4">@mdo</td>
-                            </tr>
-                            <tr class="border-b dark:border-neutral-500">
-                                <td class="whitespace-nowrap px-6 py-4 font-medium">2</td>
-                                <td class="whitespace-nowrap px-6 py-4">Jacob</td>
-                                <td class="whitespace-nowrap px-6 py-4">Thornton</td>
-                                <td class="whitespace-nowrap px-6 py-4">@fat</td>
-                            </tr>
-                            <tr class="border-b dark:border-neutral-500">
-                                <td class="whitespace-nowrap px-6 py-4 font-medium">3</td>
-                                <td class="whitespace-nowrap px-6 py-4">Larry</td>
-                                <td class="whitespace-nowrap px-6 py-4">Wild</td>
-                                <td class="whitespace-nowrap px-6 py-4">@twitter</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    </div>
-    <h1
-        ref="typingText"
-        class="overflow-hidden whitespace-nowrap font-mono text-xl font-bold animate-typing border-r-4"
-    >
-        Hello, world.
-    </h1>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
+import { ref, computed, onMounted, watch } from 'vue'
+
+const taxBrackets = [
+    { upTo: 20000, rate: 0 },
+    { upTo: 30000, rate: 0.02 },
+    { upTo: 40000, rate: 0.035 },
+    { upTo: 80000, rate: 0.07 },
+    { upTo: 120000, rate: 0.115 },
+    { upTo: 160000, rate: 0.15 },
+    { upTo: 200000, rate: 0.18 },
+    { upTo: 240000, rate: 0.19 },
+    { upTo: 280000, rate: 0.195 },
+    { upTo: 320000, rate: 0.2 },
+    { upTo: 500000, rate: 0.22 },
+    { upTo: 1000000, rate: 0.23 },
+    { upTo: Infinity, rate: 0.24 }
+]
 
 const typingText = ref<HTMLInputElement | null>(null)
+const isCalculated = ref(false)
+const income = ref(0)
+const taxRate = ref(0)
 
-// onMounted(() => {
-//     typingText.value?.focus()
-// })
+const taxAmount = computed(() => {
+    let taxPayable = 0
+    let prevBracketUpper = 0
+
+    for (const bracket of taxBrackets) {
+        const taxableInBracket = Math.min(
+            income.value - prevBracketUpper,
+            bracket.upTo - prevBracketUpper
+        )
+
+        taxPayable += taxableInBracket * bracket.rate
+
+        prevBracketUpper = bracket.upTo
+
+        if (income.value <= bracket.upTo) {
+            break
+        }
+    }
+
+    return taxPayable
+})
+
+watch(income, () => {
+    isCalculated.value = false
+})
+
+function calculateTax() {
+    taxAmount.value
+    isCalculated.value = true
+}
+
 onMounted(() => {
     typingText.value?.addEventListener('animationend', () => {
         typingText.value?.classList.remove('border-r-4', 'border-r-black')
