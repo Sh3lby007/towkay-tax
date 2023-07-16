be356e43d6f81455d76451fd714ee614d037fe11
Sh3lby007 <zhengshaobin@ymail.com>
2023-07-16 17:09:07 +0800

    Change base:mode url name:
    Before was Towkay-Tax, but github project name is towkay-tax,
    not capital letters so not showing anything.

--- vite.config.ts
+++ vite.config.ts
@@ -31,6 +31,6 @@ export default defineConfig(({ mode }) => {
         // When deploying to Github pages, the base URL will be your repo's name,
         // Thus the production base URL must be changed here for it to work when deployed.
         // Ref: https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site#next-steps
-        base: mode === 'github-pages' ? '/Towkay-Tax/' : '/'
+        base: mode === 'github-pages' ? '/towkay-tax/' : '/'
     }
 })
