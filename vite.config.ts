8b83d1fbb8ddeb7c0d51fc2189f1569252c8fad9
Sh3lby007 <zhengshaobin@ymail.com>
2023-07-16 16:35:47 +0800

    More PWA additions settings to make it work.

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,34 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
+import basicSSL from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        vue(),
-        VitePWA({
-            registerType: 'autoUpdate'
-        })
-    ],
-    resolve: {
-        alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url))
-        }
+export default defineConfig(({ mode }) => {
+    return {
+        plugins: [
+            vue(),
+            VitePWA({
+                registerType: 'autoUpdate',
+                // Manifest JSON
+                manifest: {
+                    name: 'Towkay-tax'
+                }
+            }),
+            // Enable https by default
+            // https://vitejs.dev/config/server-options.html#server-https
+            // https://vitejs.dev/guide/migration.html#automatic-https-certificate-generation
+            basicSSL()
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+
+        // When deploying to Github pages, the base URL will be your repo's name,
+        // Thus the production base URL must be changed here for it to work when deployed.
+        // Ref: https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site#next-steps
+        base: mode === 'github-pages' ? '/Towkay-Tax/' : '/'
     }
 })
